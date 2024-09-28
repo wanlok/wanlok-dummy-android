a5297f9542d2976843527c3e02c3bff512a52603
Robert Wan <wantunglok@gmail.com>
2024-09-28 10:16:37 +1000

    test date picker dialog theming

--- app/src/main/java/com/example/wanlok_dummy_android/MainActivity.java
+++ app/src/main/java/com/example/wanlok_dummy_android/MainActivity.java
@@ -1,24 +1,46 @@
 package com.example.wanlok_dummy_android;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnDatePicker;
+    private TextView tvSelectedDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnDatePicker = findViewById(R.id.btnDatePicker);
+        tvSelectedDate = findViewById(R.id.tvSelectedDate);
+
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        tvSelectedDate.setText(year + "/" + String.format("%02d", month + 1) + "/" + day);
+
+        btnDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        tvSelectedDate.setText(year + "/" + String.format("%02d", month + 1) + "/" + day);
+                    }
+                }, year, month, day).show();
+            }
         });
     }
 }
